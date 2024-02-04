e86ed8525f13e9189e5a02f88e2d87a77014dfa2
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-02-04 22:21:48 +0900

    ✨ [Feat] 재사용 가능한 토스트 컴포넌트 개발
    에러 발생시 하단에 띄울 토스트 작성, 에러코드와 메세지 인자받음

---
+++ src/components/shared/Toast.jsx
@@ -0,0 +1,60 @@
+import styled, { keyframes } from "styled-components";
+
+import { useEffect } from "react";
+
+function ToastPopup({ message, setToast, status }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setToast(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setToast]);
+
+  return (
+    <Toast>
+      {status ? (
+        <p>{`status code: ${status} messags: ${message}`}</p>
+      ) : (
+        <p>{message}</p>
+      )}
+    </Toast>
+  );
+}
+
+const fadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const Toast = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 85%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 900px;
+  height: 70px;
+  font-weight: 700;
+  border-radius: 20px;
+  animation: ${fadeInUp} 0.5s ease-in-out;
+
+  opacity: 0.8;
+  background-color: #000000;
+  color: #ffffff;
+  font-family: "Noto Sans KR";
+  font-size: 18px;
+  text-align: center;
+`;
+
+export default ToastPopup;
