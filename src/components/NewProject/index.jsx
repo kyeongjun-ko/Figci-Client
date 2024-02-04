895b132cdcf8e731f60a5b838baa05cee8e501af
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-02-04 13:48:37 +0900

    ✨ [Feat] figma 사용자인증후 인증 결과 전달 콜백 페이지 추가
    사용자 인증 후 token 요청할수 있는 코드를 전달할 콜백 페이지 추가

--- src/components/NewProject/index.jsx
+++ src/components/NewProject/index.jsx
@@ -1,4 +1,16 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+import { getToken, login } from "../../services/auth";
+
 function NewProejct() {
+  const location = useLocation();
+  const query = location.search;
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const code = searchParams.get("code");
+  const state = searchParams.get("state");
+
+  console.log("code", getToken(code));
+
   return <h1>NewProject Page</h1>;
 }
 
