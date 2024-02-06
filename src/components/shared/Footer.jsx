a9cf76ce1baaf7140a13bb6617aef689fce1c4f3
seulgi chu <seulgichu14@gmail.com>
2024-02-06 09:36:54 +0900

    ✨ [Feat] Footer 컴포넌트 추가
    Footer 컴포넌트 추가

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+import Button from "./Button";
+
+function Footer({ buttons }) {
+  return (
+    <StyledFooter>
+      {buttons.map(button => (
+        <Button
+          key={button.text}
+          onClick={button.handleClick}
+          size="medium"
+          usingCase={button.usingCase}
+        >
+          {button.text}
+        </Button>
+      ))}
+    </StyledFooter>
+  );
+}
+
+const StyledFooter = styled.footer`
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 0px;
+
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  padding: 24px 40px;
+  border-top: 2px solid;
+`;
+
+export default Footer;
