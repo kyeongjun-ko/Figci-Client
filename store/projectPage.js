1f1b53e4c3e00f09494819e9d091a0a9bf0125ec
kyeong-ko <rhrudwnszoq@naver.com>
2024-02-06 11:37:44 +0900

    ✨ [Feat] 비교 선택 가능한 페이지 상태 정규화
    비교 선택 가능한 페이지 목록 렌더링을 위해 페이지 트리에서 데이터 정규화

--- store/projectPage.js
+++ store/projectPage.js
@@ -9,15 +9,26 @@ const pageStore = set => {
       const pageIds = [];
 
       pageList.forEach(item => {
+<<<<<<< HEAD
         const { pageId, pageName } = item;
 
         pageIds.push({ [pageId]: pageName });
+=======
+        const { pageId, name, _id } = item;
+
+        pageIds.push(_id);
+>>>>>>> ✨ [Feat] 비교 선택 가능한 페이지 상태 정규화
 
         set(state => {
           state.byPages = {
             ...state.byPages,
+<<<<<<< HEAD
             [pageId]: {
               name: pageName,
+=======
+            [_id]: {
+              name,
+>>>>>>> ✨ [Feat] 비교 선택 가능한 페이지 상태 정규화
               node_id: pageId,
             },
           };
