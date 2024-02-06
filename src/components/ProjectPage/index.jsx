c8b1bff63e1782a7d7803423b991a2d7859cd20b
seulgi chu <seulgichu14@gmail.com>
2024-02-06 14:23:15 +0900

    ✨ [Feat] 레이아웃 내 컴포넌트 추가
    ProjectPage, ProjectVersion 내 컴포넌트 추가

--- src/components/ProjectPage/index.jsx
+++ src/components/ProjectPage/index.jsx
@@ -1,21 +1,39 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 import Modal from "../shared/Modal";
 import Loading from "../shared/Loading";
+import Title from "../shared/Title";
+import Select from "../shared/Select";
+import Footer from "../shared/Footer";
 
 function ProjectPage() {
   const [isLoaded, setIsLoaded] = useState(true);
 
   return (
-    <>
-      <h1>ProjectPage</h1>
+    <StyledContent>
       {!isLoaded && (
         <Modal>
           <Loading />
         </Modal>
       )}
-    </>
+      <div className="content">
+        <Title />
+        <Select />
+      </div>
+      <Footer />
+    </StyledContent>
   );
 }
+
+const StyledContent = styled.div`
+  .content {
+    box-sizing: border-box;
+
+    width: 100%;
+    height: 100%;
+    padding: 64px;
+  }
+`;
 
 export default ProjectPage;
--- src/components/ProjectVersion/index.jsx
+++ src/components/ProjectVersion/index.jsx
@@ -1,5 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+import Title from "../shared/Title";
+import Select from "../shared/Select";
+import Footer from "../shared/Footer";
+
 function ProjectVersion() {
-  return <h1>ProjectVersion</h1>;
+  return (
+    <StyledContent>
+      <div className="content">
+        <Title />
+        <Select />
+        <Select />
+      </div>
+      <Footer />
+    </StyledContent>
+  );
 }
+
+const StyledContent = styled.div`
+  .content {
+    box-sizing: border-box;
+
+    width: 100%;
+    height: 100%;
+    padding: 64px;
+  }
+`;
 
 export default ProjectVersion;
