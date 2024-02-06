19fae5b97c188c16505bb595c8ef401f838e39f5
seulgi chu <seulgichu14@gmail.com>
2024-02-06 13:59:37 +0900

    🐛 [Fix] 프로젝트 주소 입력 페이지 레이아웃 추가
    프로젝트 주소 입력 페이지 레이아웃 추가

--- src/components/NewProject/index.jsx
+++ src/components/NewProject/index.jsx
@@ -2,14 +2,19 @@
 
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import styled, { css } from "styled-components";
 
 import Modal from "../shared/Modal";
 import Welcome from "../Welcome";
+import Title from "../shared/Title";
+import Input from "../shared/Input";
+import Footer from "../shared/Footer";
 
 import { getToken } from "../../services/auth";
 
 function NewProject() {
   const [isModalOpened, setIsModalOpened] = useState(true);
+  const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
   const [query] = useSearchParams();
 
@@ -50,16 +55,64 @@ function NewProject() {
     setIsModalOpened(false);
   }
 
+  const handleInputChange = ev => {
+    setInputValue(ev.target.value);
+  };
+
+  const handleSubmitInputValue = ev => {
+    ev.preventDefault();
+
+    navigate("/version");
+  };
+
+  const contents = {
+    title: {
+      step: "01",
+      firstSentence: "디자인 변경사항을 확인할",
+      secondSentence: "피그마 프로젝트 URL을 입력해주세요.",
+    },
+    inputInfo: {
+      id: "project",
+      label: "피그마 프로젝트 URL 입력",
+      inputs: [
+        {
+          id: "address",
+          placeholder: "url 주소를 입력해주세요. (예: www.figma.com/abc)",
+        },
+      ],
+    },
+    buttons: [
+      { text: "다음", usingCase: "solid", handleClick: handleSubmitInputValue },
+    ],
+  };
+
   return (
-    <>
-      <h1>hello</h1>
+    <StyledContent>
       {isModalOpened && (
         <Modal>
           <Welcome handleClick={handleClick} />
         </Modal>
       )}
-    </>
+      <div className="content">
+        <Title title={contents.title} />
+        <Input
+          inputInfo={contents.inputInfo}
+          onInputChange={handleInputChange}
+        />
+      </div>
+      <Footer buttons={contents.buttons} />
+    </StyledContent>
   );
 }
+
+const StyledContent = styled.div`
+  .content {
+    box-sizing: border-box;
+
+    width: 100%;
+    height: 100%;
+    padding: 64px;
+  }
+`;
 
 export default NewProject;
