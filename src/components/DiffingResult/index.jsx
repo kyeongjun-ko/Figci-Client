d2e06fffb16e1bea50a347efc7c38ea207a8d12d
seulgi chu <seulgichu14@gmail.com>
2024-02-08 00:17:21 +0900

    💄 [Style] DiffingResult 스타일 설정
    DiffingResult 스타일 설정

--- src/components/DiffingResult/index.jsx
+++ src/components/DiffingResult/index.jsx
@@ -57,6 +57,76 @@ function DiffingResult() {
 const ResultWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  .result-header {
+    display: flex;
+  }
+
+  .header-information {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 24px 40px;
+    display: flex;
+    align-items: center;
+
+    border-bottom: 2px solid #000000;
+  }
+
+  .versions {
+    box-sizing: border-box;
+
+    margin-left: 24px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .content {
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: row;
+  }
+
+  .profile {
+    display: flex;
+    align-items: center;
+  }
+
+  .label {
+    margin-right: 12px;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 24px;
+  }
+
+  .version {
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 24px;
+  }
+
+  .logo-content {
+    width: 305px;
+    padding: 24px 40px;
+
+    border-right: 2px solid #000000;
+    border-bottom: 2px solid #000000;
+  }
+
+  .logo {
+    width: 70px;
+    margin-right: 21px;
+  }
+
+  .line {
+    width: 2px;
+    height: 20px;
+
+    background-color: #000000;
+  }
 `;
 
 export default DiffingResult;
