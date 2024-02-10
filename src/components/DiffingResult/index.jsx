1279d0885d2cffbbc0ddcd0984a89a67dd634f02
seulgi chu <seulgichu14@gmail.com>
2024-02-10 15:19:53 +0900

    🐛 [Fix] DiffingResult 구조 및 오류 수정
    DiffingResult 레이아웃 수정 및 fetch 오류 수정

--- src/components/DiffingResult/index.jsx
+++ src/components/DiffingResult/index.jsx
@@ -3,18 +3,75 @@ import { Link } from "react-router-dom";
 import { fabric } from "fabric";
 import styled from "styled-components";
 
+import Modal from "../shared/Modal";
+import Loading from "../shared/Loading";
 import Sidebar from "../Sidebar";
+import Button from "../shared/Button";
 
+import usePageStatusStore from "../../../store/projectInit";
+import usePageListStore from "../../../store/projectPage";
+import useProjectVersionStore from "../../../store/projectVersion";
+import getDiffingResult from "../../../services/getDiffingResult";
 import figciLogo from "../../../assets/logo_figci.jpg";
 
 function DiffingResult() {
-  const [canvas, setCanvas] = useState("");
-  const [frameUrl, setFrameUrl] = useState("");
+  const [canvas, setCanvas] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [frameList, setFrameList] = useState([]);
+
+  const { byPageIds } = usePageListStore();
+  const {
+    projectKey,
+    beforeDate,
+    beforeVersion,
+    afterDate,
+    afterVersion,
+    pageId,
+  } = usePageStatusStore(state => state.status);
+  const versionStaus = useProjectVersionStore(state => state.byDates);
+
+  const getVersionLabel = (date, versionId) => {
+    if (versionStaus[date] && versionStaus[date][versionId]) {
+      return `${date} ${versionStaus[date][versionId].label}`;
+    }
+
+    return null;
+  };
+
+  const beforeVersionLabel = getVersionLabel(beforeDate, beforeVersion);
+  const afterVersionLabel = getVersionLabel(afterDate, afterVersion);
+  const pageInfo = { id: "pageId", name: byPageIds[pageId] };
+
+  useEffect(() => {
+    const fetchDiffingResult = async () => {
+      setIsLoaded(true);
+
+      const diffingResult = await getDiffingResult(
+        projectKey,
+        beforeVersion,
+        afterVersion,
+        pageId,
+      );
+
+      const frames = diffingResult.content.frames.map(frame => {
+        return {
+          name: frame.name,
+          id: frame.frameId,
+        };
+      });
+
+      setFrameList(frames);
+
+      setIsLoaded(false);
+    };
+
+    fetchDiffingResult();
+  }, []);
 
   const initCanvas = () =>
     new fabric.Canvas("canvas", {
-      height: 1000,
-      width: 800,
+      width: 400,
+      height: 400,
       backgroundColor: "#CED4DA",
     });
 
@@ -23,34 +80,56 @@ function DiffingResult() {
   }, []);
 
   return (
-    <ResultWrapper>
-      <header className="result-header">
-        <div className="logo-content">
-          <Link to="/">
-            <img className="logo" src={figciLogo} alt="figci-logo-img" />
-          </Link>
+    <>
+      {isLoaded && (
+        <Modal>
+          <Loading />
+        </Modal>
+      )}
+      <ResultWrapper>
+        <header className="result-header">
+          <div className="logo-content">
+            <Link to="/">
+              <img className="logo" src={figciLogo} alt="figci-logo-img" />
+            </Link>
+          </div>
+          <div className="header-information">
+            <div className="version-information">
+              <div className="versions">
+                <p className="label">Before</p>
+                <p className="version">{beforeVersionLabel}</p>
+              </div>
+              <div className="versions">
+                <p className="label">After</p>
+                <p className="version">{afterVersionLabel}</p>
+              </div>
+              <Button
+                className="reselect_version"
+                size="small"
+                usingCase="line"
+              >
+                버전 재선택
+              </Button>
+            </div>
+            <div className="profile">
+              <div className="line" />
+              <div className="profile-image"></div>
+              <p className="username">username</p>
+            </div>
+          </div>
+        </header>
+        <div className="content">
+          {frameList && (
+            <Sidebar
+              page={pageInfo}
+              framesInfo={frameList}
+              frameCount={frameList.length}
+            />
+          )}
+          <canvas id="canvas" width="100" />
         </div>
-        <div className="header-information">
-          <div className="versions">
-            <p className="label">Before</p>
-            <span className="version"></span>
-          </div>
-          <div className="versions">
-            <p className="label">After</p>
-            <span className="version"></span>
-          </div>
-          <button>버전 재선택</button>
-          <div className="line"></div>
-          <div className="profile">
-            <p>username</p>
-          </div>
-        </div>
-      </header>
-      <div className="content">
-        <Sidebar className="sidebar" />
-        <canvas id="canvas" />
-      </div>
-    </ResultWrapper>
+      </ResultWrapper>
+    </>
   );
 }
 
@@ -60,22 +139,32 @@ const ResultWrapper = styled.div`
 
   .result-header {
     display: flex;
+    justify-content: space-between;
   }
 
   .header-information {
     box-sizing: border-box;
+
     width: 100%;
     padding: 24px 40px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
 
     border-bottom: 2px solid #000000;
   }
 
-  .versions {
+  .version-information {
     box-sizing: border-box;
 
-    margin-left: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .versions {
+    box-sizing: border-box;
+    margin-right: 24px;
     display: flex;
     flex-direction: row;
   }
@@ -92,6 +181,18 @@ const ResultWrapper = styled.div`
     align-items: center;
   }
 
+  .profile-image {
+    width: 48px;
+    height: 48px;
+    margin-left: 40px;
+    margin-right: 12px;
+
+    border-radius: 48px;
+    border: 2px solid #000000;
+
+    background-color: #ffffff;
+  }
+
   .label {
     margin-right: 12px;
 
@@ -102,18 +203,51 @@ const ResultWrapper = styled.div`
   }
 
   .version {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
     font-size: 1rem;
     font-style: normal;
     font-weight: 500;
-    line-height: 24px;
+    text-align: left;
+  }
+
+  .username {
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 700;
+    text-align: left;
   }
 
   .logo-content {
+    box-sizing: border-box;
+
     width: 305px;
+    min-width: 305px;
     padding: 24px 40px;
+    display: flex;
+    align-items: center;
 
     border-right: 2px solid #000000;
     border-bottom: 2px solid #000000;
+  }
+
+  .reselect_version {
+    box-sizing: border-box;
+
+    padding: 8px 16px;
+
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+    border-radius: 24px;
+    white-space: pre-line;
+    border: 2px solid #000000;
+    background-color: #ffffff;
+
+    color: #000000;
   }
 
   .logo {
