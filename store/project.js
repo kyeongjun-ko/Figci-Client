41675209c2ce9dace4d46797e4a87c0028cc7aa4
tjd985 <tjd985@gmail.com>
2024-02-13 21:04:06 +0900

    ♻️ [Refactor] 스토어 재구성
    project init 스토어 삭제 후 project스토어 재구성

---
+++ store/project.js
@@ -0,0 +1,61 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+const projectStore = set => ({
+  project: {
+    username: null,
+    userProfile: null,
+    projectUrl: null,
+    projectKey: null,
+    beforeDate: null,
+    beforeVersion: null,
+    afterDate: null,
+    afterVersion: null,
+    pageId: null,
+  },
+  setProject: newProject =>
+    set(state => ({
+      ...state,
+      project: {
+        ...state.project,
+        ...newProject,
+      },
+    })),
+  clearPageId: () =>
+    set(state => ({
+      ...state,
+      project: {
+        ...state.project,
+        pageId: null,
+      },
+    })),
+  clearVersion: () =>
+    set(state => ({
+      ...state,
+      project: {
+        ...state.project,
+        beforeDate: null,
+        beforeVersion: null,
+        afterDate: null,
+        afterVersion: null,
+        pageId: null,
+      },
+    })),
+  clearProject: () =>
+    set(state => ({
+      ...state,
+      project: {
+        projectUrl: null,
+        projectKey: null,
+        beforeDate: null,
+        beforeVersion: null,
+        afterDate: null,
+        afterVersion: null,
+        pageId: null,
+      },
+    })),
+});
+
+const useProjectStore = create(devtools(projectStore));
+
+export default useProjectStore;
