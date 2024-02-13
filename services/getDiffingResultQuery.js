85bc7a3b9a82d9f590773bd715d7e1fae247834e
tjd985 <tjd985@gmail.com>
2024-02-13 22:14:28 +0900

    ♻️ [Refactor] 모듈 통합
    getDiffingResult모듈 삭제 후 getDiffingResultQuery.js로 통합

--- services/getDiffingResultQuery.js
+++ services/getDiffingResultQuery.js
@@ -1,12 +1,44 @@
 import { useQuery } from "react-query";
-import getDiffingResult from "./getDiffingResult";
+import generateApiUri from "../utils/generateURI";
 
-function getDiffingResultQuery(
+const getDiffingResult = async (
   projectKey,
   beforeVersion,
   afterVersion,
   pageId,
-) {
+) => {
+  const baseURI = import.meta.env.VITE_BACKEND_BASE_API_URI;
+  const token = JSON.parse(localStorage.getItem("FigmaToken")).access_token;
+
+  const queryParams = {
+    "before-version": beforeVersion,
+    "after-version": afterVersion,
+  };
+
+  const API_URL = generateApiUri(
+    baseURI,
+    `/projects/${projectKey}/pages/${pageId}`,
+    queryParams,
+  );
+
+  const response = await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      authorization: token,
+    },
+  });
+
+  const responseResult = await response.json();
+
+  return responseResult;
+};
+
+const getDiffingResultQuery = (
+  projectKey,
+  beforeVersion,
+  afterVersion,
+  pageId,
+) => {
   return useQuery(
     `${beforeVersion}-${afterVersion}-${pageId}`,
     async () => {
@@ -25,6 +57,6 @@ function getDiffingResultQuery(
       enabled: !!pageId,
     },
   );
-}
+};
 
 export default getDiffingResultQuery;
