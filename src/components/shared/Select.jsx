3f6be15c896c7b009eac418e96e62abd4268c3de
tjd985 <tjd985@gmail.com>
2024-02-25 16:32:24 +0900

    ✏️ [Style] CSS단위 수정
    rem -> px로 단위 수정

--- src/components/shared/Select.jsx
+++ src/components/shared/Select.jsx
@@ -49,7 +49,7 @@ const SelectWrapper = styled.div`
     background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' %3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
     background-repeat: no-repeat;
     background-position: right 1rem top 1rem;
-    background-size: 2rem;
+    background-size: 32px;
   }
 
   .label {
