508d349b868b1d1572a40085c5fb4ca883f46680
tjd985 <tjd985@gmail.com>
2024-02-25 21:29:06 +0900

    📦️ [Chore] test config 설정
    테스팅을 위한 config 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,23 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    collectCoverageFrom: ["src/**/*.jsx", "src/**/*.js"],
+    setupFiles: "src/spec/setupTests.js",
+    testMatch: ["./src/spec/*.spec.jsx"],
+    environment: "jsdom",
+    coverage: {
+      all: true,
+      exclude: [
+        "*.config.js",
+        "*.cjs",
+        "**/main.jsx",
+        "**/atoms.js",
+        "**/constant.js",
+      ],
+    },
+  },
   plugins: [react()],
   server: {
     proxy: {
