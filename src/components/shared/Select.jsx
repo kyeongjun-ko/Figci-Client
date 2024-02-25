e79323c054e876ba56aaf766ed1da469592cad34
tjd985 <tjd985@gmail.com>
2024-02-25 16:22:13 +0900

    💄 [Design] SelectBox 화살표 UI 수정
    기존에 오른쪽에 붙어있던 화살표 UI를 수정했습니다

--- src/components/shared/Select.jsx
+++ src/components/shared/Select.jsx
@@ -45,6 +45,11 @@ const SelectWrapper = styled.div`
     font-style: normal;
     font-weight: 500;
     line-height: 28px;
+    appearance: none;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' %3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-position: right 1rem top 1rem;
+    background-size: 2rem;
   }
 
   .label {
