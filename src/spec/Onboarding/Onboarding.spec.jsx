f98755a8c71b2b88bd0d5fe60573cdd16b4549a8
tjd985 <tjd985@gmail.com>
2024-02-26 23:05:57 +0900

    ✅ [Test] Onboarding test code 작성
    온보딩 컴포넌트 테스트 코드 작성

--- src/spec/Onboarding/Onboarding.spec.jsx
+++ src/spec/Onboarding/Onboarding.spec.jsx
@@ -13,24 +13,24 @@ describe("Onboarding Component Test", () => {
   });
 
   it("온보딩 페이지에서 메인설명이 렌더 되어야 합니다", () => {
-    const mainDescription = screen.getByText(
+    const mainDescriptionElement = screen.getByText(
       /피그마 계정으로 로그인하시면 파일버전을 비교해*디자인 화면의 변경사항을 쉽게 보여드려요!/,
     );
 
-    expect(mainDescription.toBeInTheDocument);
+    expect(mainDescriptionElement).toBeInTheDocument();
   });
 
   it("온보딩 페이지에서 서브설명이 렌더 되어야 합니다", () => {
-    const subDescription = screen.getByText(
+    const subDescriptionElement = screen.getByText(
       /비교할 두 피그마 버전을 알려주시면*버전 별 디자인 화면 변경사항을 바로 알려드려요/,
     );
 
-    expect(subDescription.toBeInTheDocument);
+    expect(subDescriptionElement).toBeInTheDocument;
   });
 
   it("온보딩 페이지에서 로그인 버튼이 렌더 되어야 합니다", () => {
-    const loginButtonEl = screen.getByText("피그마 계정으로 로그인");
+    const loginButtonElement = screen.getByText("피그마 계정으로 로그인");
 
-    expect(loginButtonEl.type).toBe("submit");
+    expect(loginButtonElement.type).toBe("submit");
   });
 });
