0bb32c2f4824484d1b7e9beb6f336f471ac0e2bd
seulgi chu <seulgichu14@gmail.com>
2024-02-26 23:27:59 +0900

    ✨ [Feat] ProjectPage 테스트 케이스 추가
    ProjectPage 테스트 케이스 추가

--- src/spec/ProjectPage/ProjectPage.spec.jsx
+++ src/spec/ProjectPage/ProjectPage.spec.jsx
@@ -43,3 +43,32 @@ beforeEach(() => {
 afterEach(() => {
   cleanup();
 });
+
+describe("ProjectPage Component Test", () => {
+  it("글로벌 상태에 저장된 프로젝트의 공통된 페이지들을 가져와서 렌더링 해야 합니다", () => {
+    const selectBoxElement = screen.getByRole("combobox");
+
+    fireEvent.click(selectBoxElement);
+
+    const optionsElements = screen.getAllByRole("option");
+
+    expect(optionsElements[0].textContent).toBe(allPages[0].pageName);
+    expect(optionsElements[1].textContent).toBe(allPages[1].pageName);
+  });
+
+  it("비교 페이지 선택 페이지에서 타이틀과 셀렉트가 렌더 되어야 합니다", () => {
+    const titleElement = screen.getByText("비교할 페이지를 선택해주세요.");
+    const selectBoxElement = screen.getByRole("combobox");
+
+    expect(titleElement).toBeInTheDocument();
+    expect(selectBoxElement).toBeInTheDocument();
+  });
+
+  it("비교 페이지 선택 페이지에서 이전, 비교하기 버튼이 렌더 되어야 합니다", () => {
+    const backButtonElement = screen.getByText("이전");
+    const compareButtonElement = screen.getByText("비교하기");
+
+    expect(backButtonElement).toBeInTheDocument();
+    expect(compareButtonElement).toBeInTheDocument();
+  });
+});
